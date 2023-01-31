161191fc1aed2028b191f12b0371467bffe43a64
Miłosz <milosz.devmail@gmail.com>
2023-02-01 00:03:27 +0100

    Add title to form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import { addCountry } from "../../store/tableSlice";
+import "./Form.scss";
 
 const FormSchema = Yup.object().shape({
   country: Yup.string()
@@ -24,50 +25,55 @@ const CountryForm: FC = () => {
   const dispatch = useDispatch();
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={FormSchema}
-      onSubmit={(values) => {
-        const formattedValues = {
-          ...values,
-          golden: Number(values.golden),
-          silver: Number(values.silver),
-          bronze: Number(values.bronze),
-        };
+    <div className="form-wrapper">
+      <div className="title-wrapper">
+        <p>Add country</p>
+      </div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={FormSchema}
+        onSubmit={(values) => {
+          const formattedValues = {
+            ...values,
+            golden: Number(values.golden),
+            silver: Number(values.silver),
+            bronze: Number(values.bronze),
+          };
 
-        dispatch(addCountry(formattedValues));
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <input
-            name="country"
-            value={values.country}
-            onChange={handleChange}
-            placeholder="Poland"
-          />
-          <input
-            name="golden"
-            value={values.golden}
-            onChange={handleChange}
-            placeholder="0"
-          />
-          <input
-            name="silver"
-            value={values.silver}
-            onChange={handleChange}
-            placeholder="0"
-          />
-          <input
-            name="bronze"
-            value={values.bronze}
-            onChange={handleChange}
-            placeholder="0"
-          />
-          <button type="submit">Add</button>
-        </Form>
-      )}
-    </Formik>
+          dispatch(addCountry(formattedValues));
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <input
+              name="country"
+              value={values.country}
+              onChange={handleChange}
+              placeholder="Poland"
+            />
+            <input
+              name="golden"
+              value={values.golden}
+              onChange={handleChange}
+              placeholder="0"
+            />
+            <input
+              name="silver"
+              value={values.silver}
+              onChange={handleChange}
+              placeholder="0"
+            />
+            <input
+              name="bronze"
+              value={values.bronze}
+              onChange={handleChange}
+              placeholder="0"
+            />
+            <button type="submit">Add</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
