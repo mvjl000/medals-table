6ced733dce4f2ecbe052dedf03f31706da6c76a1
Miłosz <milosz.devmail@gmail.com>
2023-01-31 23:39:44 +0100

    Add addCountry dispatch action

--- src/store/tableSlice.ts
+++ src/store/tableSlice.ts
@@ -51,11 +51,12 @@ export const tableSlice = createSlice({
   name: "table",
   initialState,
   reducers: {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
     addCountry: (state, action: PayloadAction<Country>) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
+      state.countries.push(action.payload);
     },
   },
 });
