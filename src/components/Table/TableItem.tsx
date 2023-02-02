c8e0f6686cfbba909da7902cf83c885aa6624031
Miłosz <milosz.devmail@gmail.com>
2023-02-02 15:08:40 +0100

    Remove delete function from table item

--- src/components/Table/TableItem.tsx
+++ src/components/Table/TableItem.tsx
@@ -1,13 +1,9 @@
 import { FC } from "react";
 import { useDispatch } from "react-redux";
-import { Country, openModal, removeCountry } from "../../store/tableSlice";
+import { Country, openModal } from "../../store/tableSlice";
 
 const TableItem: FC<{ data: Country; index: number }> = ({ data, index }) => {
   const dispatch = useDispatch();
-
-  const handleButtonClick = (id: string) => {
-    dispatch(removeCountry({ id }));
-  };
 
   return (
     <tr onClick={() => dispatch(openModal({ country: data }))}>
@@ -17,16 +13,6 @@ const TableItem: FC<{ data: Country; index: number }> = ({ data, index }) => {
       <td>{data.silver}</td>
       <td>{data.bronze}</td>
       <td>{data.total}</td>
-      {/* <td>
-        <button
-          className="row-button"
-          type="button"
-          aria-label="Action button"
-          onClick={() => handleButtonClick(data.id)}
-        >
-          X
-        </button>
-      </td> */}
     </tr>
   );
 };
